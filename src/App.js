7568d09444011952a3c425d6322bf8d803bf26f8
Alisser <gnlrjswjd@naver.com>
2024-10-08 16:38:30 +0900

    api_get

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    // async를 붙여 js가 이 작업이 끝나는걸 기다리게 함
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
+  }
 }
 
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Movie(){
+    return <h1>Movie</h1>
+}
+
+Movie.propTypes = {
+    
+}
